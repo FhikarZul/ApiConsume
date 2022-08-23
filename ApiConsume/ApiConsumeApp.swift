a4629664f2652abcf7b3e46191e5f2aa5ca4bcb5
Fhikar Zul <Fhikarzul18@gmail.com>
2022-08-23 14:12:21 +0800

    Initial Commit

---
+++ ApiConsume/ApiConsumeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApiConsumeApp.swift
+//  ApiConsume
+//
+//  Created by Lucy on 23/08/22.
+//
+
+import SwiftUI
+
+@main
+struct ApiConsumeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
